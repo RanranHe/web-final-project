755d92c77cd8d1c1473ac226c23dbdacb112fb85
Ranran He <cangxuexiao@gmail.com>
2019-11-19 23:35:09 -0500

    add login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import {RouterModule} from "@angular/router";
 import {AppComponent} from './app.component';
 import {NavBarComponent} from './nav-bar/nav-bar.component'
 import {MainComponent} from './main/main.component'
+import {LoginComponent} from './login/login.component'
 
 // Services
 
@@ -23,7 +24,8 @@ import { appRoutes } from './route';
   declarations: [
     AppComponent,
     NavBarComponent,
-    MainComponent
+    MainComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit, Input, Output} from '@angular/core';
+
+// import {Item} from '../models/item';
+// import {ItemService} from '../services/item.service';
+import {forEach} from '@angular/router/src/utils/collection';
+import {Observable} from 'rxjs';
+
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+export class LoginComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+}
