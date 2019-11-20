128c15e6773da57a24e8e70a5f455dd391dfb08d
Ranran He <cangxuexiao@gmail.com>
2019-11-20 18:45:11 -0500

    finish  models

--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -1,4 +1,6 @@
 import {Order} from "./order";
+import {Restaurant} from "./restaurant";
+import {Review} from "./review";
 
 export enum Role {
   USER, DELIVERYMAN, MANAGER, ADMIN
@@ -11,24 +13,21 @@ export enum WorkerStatus {
 export class User {
   username: string;
   role: Role;
-  password: String;
-  email: String;
-  firstName: String;
-  lastName: String;
-  facebook: {
-    id:    String,
-    token: String
-  };
+  password: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  facebook: FaceBook;
   dateCreated: Date;
   // For users
-  phone: String;
+  phone: string;
   orders: Array<Order>;
   // For managers
   income: Number;
   restaurants: Array<Restaurant>;
   // For DeliveryMan
   status: WorkerStatus;
-  reviews: Array<String>;
+  reviews: Array<Review>;
 
   constructor(username: string, password: string, role: Role) {
     this.username = username;
@@ -37,7 +36,17 @@ export class User {
     this.dateCreated = new Date();
     this.orders = new Array<Order>();
     this.restaurants = new Array<Restaurant>();
-    this.reviews = new Array<String>();
+    this.reviews = new Array<Review>();
     this.status = WorkerStatus.FREE;
+  }
+}
+
+export class FaceBook {
+  id:    string;
+  token: string;
+
+  constructor(id: string, token: string) {
+    this.id = id;
+    this.token = token;
   }
 }
