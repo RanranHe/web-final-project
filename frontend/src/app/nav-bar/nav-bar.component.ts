13de1be86acc5bfea48dea408f210dd3be1859d4
Ranran He <cangxuexiao@gmail.com>
2019-11-25 19:16:30 -0500

    margin of nav-bar

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Input, Output} from '@angular/core';
 import {AuthenticationService} from "../services/authenticationService";
+import {Role} from "../models/user";
 
 // declare var showCart: any;
 
@@ -13,14 +14,33 @@ export class NavBarComponent implements OnInit {
   authenticationService: AuthenticationService;
   isLogin: boolean;
   currentUser = null;
+  isCustomer = false;
+  isDeliveryman = false;
+  isManager = false;
   showCart = false;
 
   constructor(authenticationService: AuthenticationService) {
     this.authenticationService = authenticationService;
     this.authenticationService.currentUser.subscribe(user => {
       if (user) {
+        console.log(user);
         this.currentUser = user;
         this.isLogin = true;
+        if (user.role === Role.DELIVERYMAN) {
+          this.isDeliveryman = true;
+          this.isCustomer = false;
+          this.isManager = false;
+        }
+        if (user.role === Role.ADMIN) {
+          this.isManager = true;
+          this.isDeliveryman = false;
+          this.isCustomer = false;
+        }
+        if (user.role === Role.CUSTOMER) {
+          this.isManager = false;
+          this.isDeliveryman = false;
+          this.isCustomer = true;
+        }
       } else {
         this.currentUser = null;
         this.isLogin = false;
@@ -37,11 +57,12 @@ export class NavBarComponent implements OnInit {
   }
 
   logout() {
-    this.authenticationService.logout();
     window.location.reload();
+    this.authenticationService.logout();
   }
 
   ngOnInit() {
+
   }
 
 }
