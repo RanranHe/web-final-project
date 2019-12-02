7ce8db4457c6d7e9ca01828294829117bea3d803
Harold Wu <wu.hao8@husky.neu.edu>
2019-12-01 21:09:47 -0500

    updateDec.1

--- frontend/src/app/restaurant-search-results/restaurant-search-results.component.ts
+++ frontend/src/app/restaurant-search-results/restaurant-search-results.component.ts
@@ -1,13 +1,7 @@
 import {Component, OnInit, Output} from '@angular/core';
-import {UserService} from '../services/userService'
 import {ActivatedRoute, Router} from '@angular/router';
-import {AuthenticationService} from "../services/authenticationService";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {catchError, ignoreElements} from "rxjs/operators";
 import {RestaurantService} from "../services/restaurantService";
 import {Restaurant} from "../models/restaurant";
-
-declare var loginFormTextControl: any;
 
 @Component({
   selector: 'search',
@@ -16,24 +10,56 @@ declare var loginFormTextControl: any;
 })
 
 export class RestaurantSearchResultsComponent implements OnInit {
+  @Output() restaurants: Array<Restaurant>;
+  @Output() showMoreBtn = false;
+
+  // search key word
   keyword: string;
-  restaurants: Array<Restaurant>;
+  // default show restaurant size
+  defaultShowRestSize = 12;
+  // data received from api
+  defaultShowRest: Array<Restaurant>;
+  restRest: Array<Restaurant>;
+  showMoreSize = 12;
+
 
   constructor(private restaurantService: RestaurantService, private router: Router, private routeInfo: ActivatedRoute) {
+    // get the search key word form routing
     this.keyword = this.routeInfo.snapshot.params.key;
-
+    // get data fit search key from api
     this.restaurantService.findFoodByLocation(this.keyword).subscribe(data =>
       // @ts-ignore
-      this.restaurants = Array.from(data.restaurants)
+      this.setup(data)
     );
   }
 
-  getUrl(data: any){
-    console.log(data);
+  // split data to "show now' and "show later"
+  setup(data) {
+    this.restaurants = Array.from(data.restaurants);
+    if (this.restaurants.length <= this.defaultShowRestSize) {
+      this.showMoreBtn = false;
+      this.defaultShowRest = this.restaurants;
+    } else {
+      this.showMoreBtn = true;
+      this.defaultShowRest = this.restaurants.slice(0, this.defaultShowRestSize);
+      this.restRest = this.restaurants.slice(this.defaultShowRestSize, this.restaurants.length);
+    }
+  }
+
+  // Once the "show more" button is clicked
+  showMore() {
+    if (this.restRest.length <= this.showMoreSize) {
+      this.showMoreBtn = false;
+      this.defaultShowRest = this.restaurants;
+    } else {
+      this.showMoreBtn = true;
+      this.defaultShowRest = this.defaultShowRest.concat(this.restRest.slice(0, this.showMoreSize));
+      this.restRest = this.restRest.slice(this.showMoreSize, this.restRest.length);
+    }
   }
 
   ngOnInit() {
-    console.log(this.restaurants);
+
   }
 
 }
