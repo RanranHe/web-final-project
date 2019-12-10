e182c5f2af6e7288bdf6d6fc59d2d7cef2955f9d
langman <langmomo147@gmail.com>
2019-12-10 08:40:33 -0500

    update order

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -44,7 +44,9 @@ export class CartComponent implements OnInit {
     console.log(this.creditCardExpireDate)
     let currOrder = new Order(this.currUser, this.address, this.foods, this.creditCard, this.creditCardHolder, "12/23/12", this.contactName, this.totalPrice, this.phone);
     currOrder.status = DeliveryStatus.Processing;
-    const order = this.orderService.createOrder(currOrder, this.currUser._id)
+    const order = this.orderService.createOrder(currOrder, this.currUser._id).subscribe(order=>{
+      console.log(order);
+    })
     this.router.navigate(["orderList"]);
 >>>>>>> updatee
   }
